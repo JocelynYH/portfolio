321c97da887907e14248c5de0ccc919f5f093630
Jocelyn Huang <seaweed4@live.com>
2018-08-06 15:41:16 -0400

    Added repsonsive footer

--- test.php
+++ test.php
@@ -1,10 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <link rel="stylesheet" href="css/stylesheet.css">
+    <link rel="stylesheet" href="css/test.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+    <script src='js/test.js' type='text/javascript'></script>
+
+    <!--    Timeline JS-->
+    <!-- 1 -->
+    <!--    <link title="timeline-styles" rel="stylesheet" href="https://cdn.knightlab.com/libs/timeline3/latest/css/timeline.css">-->
+
+
+
+
 </head>
+
 <body>
-    <div class="box"></div>
+<ul class='sm-link-container sm'>
+    <li class='sm'>
+        <a href="#" class="transparency sm"><span class='sm'>LinkedIn</span><i class="sm" data-text='github'>
+    <?php echo file_get_contents("raw/icons/001-linkedin-logo.svg"); ?>
+</i></a>
+    </li>
+    <li class='sm'>
+
+    </li>
+
+    <li class='sm'>
+        <a href="#" class="transparency sm"><span class='sm'>Github</span><i class="sm">
+     <?php echo file_get_contents("raw/icons/002-github-logo.svg"); ?></i></a>
+    </li>
+    <li class='sm'>
+        <a href="#" class="transparency sm"><span class='sm'>Resume</span><i class="sm">
+    <?php echo file_get_contents("raw/icons/003-resume.svg"); ?></i></a>
+    </li>
+    <li class='sm'>
+        <a href="#" class="transparency sm"><span class='sm'>Email</span><i class="sm">
+    <?php echo file_get_contents("raw/icons/envelope.svg"); ?></i></a>
+    </li>
+</ul>
 </body>
+
 </html>
