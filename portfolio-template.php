8b53252ee55e21d85bdf2e8ae05a5968e4928ac9
Jocelyn Huang <seaweed4@live.com>
2018-08-19 17:18:18 -0400

    update before redoing web portfolio page

--- portfolio-template.php
+++ portfolio-template.php
@@ -1,17 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
     <?php include("includes/head.html");?>
     <link rel="stylesheet" href="css/new-menu.css">
+    <link rel="stylesheet" href="css/web.css">
+    <script src='js/landing.js'></script>
+
 </head>
+
 <body>
-   
-   <nav id='fixed-nav-container'>
-       <div id='view-all'></div>
-       <div id="menu"></div>
-   </nav>
-    
+
+    <div class="web-portfolio-wrapper">
+
+        <nav id='fixed-nav-container'>
+           
+            <a href='' id='view-all' class='button-container'>
+                <h4>view all</h4>
+                <img src="raw/web/view-all-squares.svg" alt="">
+            </a>
+            
+            <div id="menu" class='button-container'>
+                <div id="new-menu">
+
+                    <div class="open">
+                        <span class="cls"></span>
+                        <span>
+                            <ul class="sub-menu ">
+                                <li>
+                                    <a href="index.php" title="Home">Home & About</a>
+                                </li>
+                                <li>
+                                    <a href="art.php" title="Art">Artwork</a>
+                                </li>
+                                <li>
+                                    <a href="web.php" title="Web">Web Portfolio</a>
+                                </li>
+                                <li>
+                                    <a href="graphicdesign.php" title="Graphics">Graphic Design Portfolio</a>
+                                </li>
+                            </ul>
+                        </span>
+                        <span class="cls"></span>
+                    </div>
+
+                </div>
+            </div>
+        </nav>
+
+        <section class='opening'>
+            
+        </section>
+
+        <section></section>
+        <section></section>
+    </div>
+
 </body>
+
 </html>
