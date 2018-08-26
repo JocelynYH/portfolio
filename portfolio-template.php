0ccebef17a7112e52108ff66497f4b64ad4e95c6
Jocelyn Huang <seaweed4@live.com>
2018-08-26 14:05:10 -0400

    portfolio template is responsive

--- portfolio-template.php
+++ portfolio-template.php
@@ -53,11 +53,12 @@
         </nav>
 
         <div class="stripes"></div>
-        <div class="circle"></div>
+        
 
 
         <!--       OPENING         -->
         <section class='opening'>
+           <div class="circle"></div>
             <div class="background-images waves">
             </div>
 
@@ -131,44 +132,32 @@
        <!--       SITE PREVIEW       -->
 
         <section class='site-preview'>
-        <img class='abs blue-blob' src="raw/web/links_small%20blue%20blob.svg" alt="">
-        <img class='abs blue-ring' src="raw/web/links_small%20blue%20ring.svg" alt="">
-        <img class='abs blue-rings-two' 
-        src="raw/web/links_two%20circles.svg" alt="">
+            <img class='abs blue-blob' src="raw/web/links_small%20blue%20blob.svg" alt="">
+            <img class='abs blue-ring' src="raw/web/links_small%20blue%20ring.svg" alt="">
+            <img class='abs blue-rings-two' 
+            src="raw/web/links_two%20circles.svg" alt="">
         
-        <div class="content-background">
-           <img class='abs large-pink-blob' src="raw/web/links_big%20pink%20blob.svg" alt="">
-            <div class="content">
-                <h2>The Recipe:</h2>
-                    <ul>
-                        <li>
-                            <p>a re-strategized UI focused on orders</p>
-                        </li>
-                        <li>
-                            <p>more space for content and boosted SEO</p>
-                        </li>
-                        <li>
-                            <p>more space for content and boosted SEO</p>
-                        </li>
-                        <li>
-                            <p>more space for content and boosted SEO</p>
-                        </li>
-                    </ul>
-                <button class='general btn btn-reverse btn-arrow'><a href=""><span><h3>See the Result</h3><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 36.1 25.8" enable-background="new 0 0 36.1 25.8" xml:space="preserve"><g><line fill="none" stroke="#FFFFFF" stroke-width="3" stroke-miterlimit="10" x1="0" y1="12.9" x2="34" y2="12.9"></line><polyline fill="none" stroke="#FFFFFF" stroke-width="3" stroke-miterlimit="10" points="22.2,1.1 34,12.9 22.2,24.7   "></polyline></g></svg></span></a></button>
-            </div>
-        </div>
-<!--
-            <div class="computer-container">
-                <img src="raw/web/Cville-mac-desktop.png" alt="" class="computer">
-                <div class="iframe-container">
-                    <iframe src="http://jocelynhuang.com/vector/" frameborder="0">
-                
-            </iframe>
+            <div class="content-background">
+               <img class='abs large-pink-blob' src="raw/web/links_big%20pink%20blob.svg" alt="">
+                <div class="content">
+                    <h2>The Recipe:</h2>
+                        <ul>
+                            <li>
+                                <p>a re-strategized UI focused on orders</p>
+                            </li>
+                            <li>
+                                <p>more space for content and boosted SEO</p>
+                            </li>
+                            <li>
+                                <p>more space for content and boosted SEO</p>
+                            </li>
+                            <li>
+                                <p>more space for content and boosted SEO</p>
+                            </li>
+                        </ul>
+                    <button class='general btn btn-reverse btn-arrow'><a href="" target = "_blank"><span><h4>See the Result</h4><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 36.1 25.8" enable-background="new 0 0 36.1 25.8" xml:space="preserve"><g><line fill="none" stroke="#FFFFFF" stroke-width="3" stroke-miterlimit="10" x1="0" y1="12.9" x2="34" y2="12.9"></line><polyline fill="none" stroke="#FFFFFF" stroke-width="3" stroke-miterlimit="10" points="22.2,1.1 34,12.9 22.2,24.7   "></polyline></g></svg></span></a></button>
                 </div>
-
             </div>
--->
-
         </section>
 
        
@@ -178,17 +167,26 @@
             <h2>Explore Next</h2>
             <div class="tiles">
                 <div class="image-container">
-                    <img src="raw/Foodio-tile.png" alt="">
+                   <a class="overlay-link" href="/art.php"><div class="overlay">
+                           <h3>African Electronic Music</h3>
+                       </div></a>
+                    <img src="raw/AEM.png" alt="">
                 </div>
                 <div class="image-container">
-                    <img src="raw/Foodio-tile.png" alt="">
+                   <a class="overlay-link" href="/art.php"><div class="overlay">
+                           <h3>Cavalier Symphony Orchestra</h3>
+                       </div></a>
+                    <img src="raw/CSO.png" alt="">
                 </div>
                 <div class="image-container">
-                    <img src="raw/Foodio-tile.png" alt="">
+                   <a class="overlay-link" href="/art.php"><div class="overlay">
+                           <h3>Cville Weekly</h3>
+                       </div></a>
+                    <img src="raw/cvilleweekly.png" alt="">
                 </div>
             </div>
 
-            <button class='general btn btn-reverse btn-arrow'><a href=""><span><h3>View All</h3><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 36.1 25.8" enable-background="new 0 0 36.1 25.8" xml:space="preserve"><g><line fill="none" stroke="#FFFFFF" stroke-width="3" stroke-miterlimit="10" x1="0" y1="12.9" x2="34" y2="12.9"></line><polyline fill="none" stroke="#FFFFFF" stroke-width="3" stroke-miterlimit="10" points="22.2,1.1 34,12.9 22.2,24.7   "></polyline></g></svg></span></a></button>
+            <button class='general btn btn-reverse btn-arrow'><a href=""><span><h4>View All</h4><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 36.1 25.8" enable-background="new 0 0 36.1 25.8" xml:space="preserve"><g><line fill="none" stroke="#FFFFFF" stroke-width="3" stroke-miterlimit="10" x1="0" y1="12.9" x2="34" y2="12.9"></line><polyline fill="none" stroke="#FFFFFF" stroke-width="3" stroke-miterlimit="10" points="22.2,1.1 34,12.9 22.2,24.7   "></polyline></g></svg></span></a></button>
         </section>
 
        
@@ -197,7 +195,7 @@
 
         <footer>
             <div class="waves-container">
-            <img src="raw/web/footer_footer.png" alt="">
+<!--                <img src="raw/web/footer_footer_footer%20waves%202.png" alt="">-->
             </div>
             <div class="content">
                 <div class="sm-links">
